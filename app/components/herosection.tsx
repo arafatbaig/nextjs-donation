28462e4945fd21bcc92293f59eb137f597ae6a27
Arafat <arafatmirza03@gmail.com>
2023-12-13 13:32:16 +0530

    did some chnage to hero

--- app/components/herosection.tsx
+++ app/components/herosection.tsx
@@ -11,14 +11,12 @@ const HeroSection = () => {
             src="/image.jpg"
             width={"640"}
             height={"100"}
-            
           />
         </div>
         <div className="lg:flex-grow md:w-1/2 lg:pl-24 md:pl-16 flex flex-col md:items-start md:text-left items-center text-center">
           <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
-            {" "}
-            "Meditation is the art of quieting the mind to discover the wisdom
-            of the heart."
+            Meditation is the art of quieting the mind to discover the wisdom
+            of the heart.
           </h1>
           <p className="mb-8 leading-relaxed">
             In the hustle and bustle of our daily lives, amidst the noise of the
